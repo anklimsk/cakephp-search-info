72b799cdec1f677d5bf0518631629a75ab5ec27d
Andrey Klimov <anklimsk@gmail.com>
2018-10-01 10:53:56 +0300

    Convert code to CakePHP Coding Standards

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -16,134 +16,113 @@ App::uses('CakeSearchInfoAppController', 'CakeSearchInfo.Controller');
  *
  * @package plugin.Controller
  */
-class SearchController extends CakeSearchInfoAppController
-{
+class SearchController extends CakeSearchInfoAppController {
 
-    /**
-     * The name of this controller. Controller names are plural, named after the model they manipulate.
-     *
-     * @var string
-     * @link http://book.cakephp.org/2.0/en/controllers.html#controller-attributes
-     */
-    public $name = 'Search';
+/**
+ * The name of this controller. Controller names are plural, named after the model they manipulate.
+ *
+ * @var string
+ * @link http://book.cakephp.org/2.0/en/controllers.html#controller-attributes
+ */
+	public $name = 'Search';
 
-    /**
-     * An array containing the class names of models this controller uses.
-     *
-     * Example: `public $uses = array('Product', 'Post', 'Comment');`
-     *
-     * Can be set to several values to express different options:
-     *
-     * - `true` Use the default inflected model name.
-     * - `array()` Use only models defined in the parent class.
-     * - `false` Use no models at all, do not merge with parent class either.
-     * - `array('Post', 'Comment')` Use only the Post and Comment models. Models
-     *   Will also be merged with the parent class.
-     *
-     * The default value is `true`.
-     *
-     * @var mixed
-     * @link http://book.cakephp.org/2.0/en/controllers.html#components-helpers-and-uses
-     */
-    public $uses = [
-        'CakeSearchInfo.Search'
-    ];
+/**
+ * An array containing the class names of models this controller uses.
+ *
+ * @var mixed
+ * @link http://book.cakephp.org/2.0/en/controllers.html#components-helpers-and-uses
+ */
+	public $uses = [
+		'CakeSearchInfo.Search'
+	];
 
-    /**
-     * Array containing the names of components this controller uses. Component names
-     * should not contain the "Component" portion of the class name.
-     *
-     * Example: `public $components = array('Session', 'RequestHandler', 'Acl');`
-     *
-     * @var array
-     * @link http://book.cakephp.org/2.0/en/controllers/components.html
-     */
-    public $components = [
-        'Paginator',
-        'CakeSearchInfo.SearchFilter'
-    ];
+/**
+ * Array containing the names of components this controller uses. Component names
+ * should not contain the "Component" portion of the class name.
+ *
+ * @var array
+ * @link http://book.cakephp.org/2.0/en/controllers/components.html
+ */
+	public $components = [
+		'Paginator',
+		'CakeSearchInfo.SearchFilter'
+	];
 
-    /**
-     * An array containing the names of helpers this controller uses. The array elements should
-     * not contain the "Helper" part of the class name.
-     *
-     * Example: `public $helpers = array('Html', 'Js', 'Time', 'Ajax');`
-     *
-     * @var mixed
-     * @link http://book.cakephp.org/2.0/en/controllers.html#components-helpers-and-uses
-     */
-    public $helpers = [
-        'Text',
-        'Number',
-        'Paginator',
-        'CakeSearchInfo.Search',
-        'CakeTheme.ViewExtension',
-    ];
+/**
+ * An array containing the names of helpers this controller uses. The array elements should
+ * not contain the "Helper" part of the class name.
+ *
+ * @var mixed
+ * @link http://book.cakephp.org/2.0/en/controllers.html#components-helpers-and-uses
+ */
+	public $helpers = [
+		'Text',
+		'Number',
+		'Paginator',
+		'CakeSearchInfo.Search',
+		'CakeTheme.ViewExtension',
+	];
 
-    /**
-     * Settings for component 'Paginator'
-     *
-     * @var array
-     */
-    public $paginate = [
-        'page' => 1,
-        'limit' => 10,
-        'maxLimit' => 250,
-    ];
+/**
+ * Settings for component 'Paginator'
+ *
+ * @var array
+ */
+	public $paginate = [
+		'page' => 1,
+		'limit' => 10,
+		'maxLimit' => 250,
+	];
 
-    /**
-     * Called before the controller action. You can use this method to configure and customize components
-     * or perform logic that needs to happen before each controller action.
-     *
-     * Actions:
-     *  - Configure components;
-     *
-     * @return void
-     * @link http://book.cakephp.org/2.0/en/controllers.html#request-life-cycle-callbacks
-     */
-    public function beforeFilter()
-    {
-        $this->Auth->allow('autocomplete');
-        $this->Security->unlockedActions = ['autocomplete'];
+/**
+ * Called before the controller action. You can use this method to configure and customize components
+ * or perform logic that needs to happen before each controller action.
+ *
+ * Actions:
+ *  - Configure components;
+ *
+ * @return void
+ * @link http://book.cakephp.org/2.0/en/controllers.html#request-life-cycle-callbacks
+ */
+	public function beforeFilter() {
+		$this->Auth->allow('autocomplete');
+		$this->Security->unlockedActions = ['autocomplete'];
 
-        parent::beforeFilter();
-    }
+		parent::beforeFilter();
+	}
 
-    /**
-     * Action `index`. Used to begin search.
-     *
-     * @return void
-     */
-    public function index()
-    {
-        $search_urlActionSearch = null;
-        $pageTitle = __d('cake_search_info', 'Search information');
-        $breadCrumbs = $this->Search->getBreadcrumbInfo();
-        $breadCrumbs[] = __d('cake_search_info', 'New search');
-        $this->set(compact('search_urlActionSearch', 'pageTitle', 'breadCrumbs'));
-    }
+/**
+ * Action `index`. Used to begin search.
+ *
+ * @return void
+ */
+	public function index() {
+		$pageTitle = __d('cake_search_info', 'Search information');
+		$breadCrumbs = $this->Search->getBreadcrumbInfo();
+		$breadCrumbs[] = __d('cake_search_info', 'New search');
+		$this->set(compact('pageTitle', 'breadCrumbs'));
+		$this->set('search_urlActionSearch', null);
+	}
 
-    /**
-     * Action `search`. Used to view a result of search.
-     *
-     * @return void
-     */
-    public function search()
-    {
-        $this->SearchFilter->search();
-        $pageTitle = __d('cake_search_info', 'Search information');
-        $breadCrumbs = $this->Search->getBreadcrumbInfo();
-        $breadCrumbs[] = __d('cake_search_info', 'Results of search');
-        $this->set(compact('pageTitle', 'breadCrumbs'));
-    }
+/**
+ * Action `search`. Used to view a result of search.
+ *
+ * @return void
+ */
+	public function search() {
+		$this->SearchFilter->search();
+		$pageTitle = __d('cake_search_info', 'Search information');
+		$breadCrumbs = $this->Search->getBreadcrumbInfo();
+		$breadCrumbs[] = __d('cake_search_info', 'Results of search');
+		$this->set(compact('pageTitle', 'breadCrumbs'));
+	}
 
-    /**
-     * Action `autocomplete`. Is used to autocomplte input fields.
-     *
-     * @return void
-     */
-    public function autocomplete()
-    {
-        $this->SearchFilter->autocomplete();
-    }
+/**
+ * Action `autocomplete`. Is used to autocomplte input fields.
+ *
+ * @return void
+ */
+	public function autocomplete() {
+		$this->SearchFilter->autocomplete();
+	}
 }
--- View/Search/index.ctp
+++ View/Search/index.ctp
@@ -1,21 +1,21 @@
 <?php
-    /**
-     * This file is the view file of the plugin. Used for begin search.
-     *
-     * CakeSearchInfo: Search information in project database
-     * @copyright Copyright 2016, Andrey Klimov.
-     * @package plugin.View.Search
-     */
+/**
+ * This file is the view file of the plugin. Used for begin search.
+ *
+ * CakeSearchInfo: Search information in project database
+ * @copyright Copyright 2016, Andrey Klimov.
+ * @package plugin.View.Search
+ */
 
-    $this->assign('title', $pageTitle);
-    $this->ViewExtension->addBreadCrumbs($breadCrumbs);
+	$this->assign('title', $pageTitle);
+	$this->ViewExtension->addBreadCrumbs($breadCrumbs);
 ?>
-    <div class="container container-table"> 
-        <div class="row vertical-center-row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+	<div class="container container-table"> 
+		<div class="row vertical-center-row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
 <?php
-    echo $this->Search->createFormSearch($search_targetFields, $search_targetFieldsSelected, $search_urlActionSearch, $search_targetDeep, $search_querySearchMinLength);
-?>          
-            </div>
-        </div>
-    </div>
+	echo $this->Search->createFormSearch($search_targetFields, $search_targetFieldsSelected, $search_urlActionSearch, $search_targetDeep, $search_querySearchMinLength);
+?>
+			</div>
+		</div>
+	</div>
